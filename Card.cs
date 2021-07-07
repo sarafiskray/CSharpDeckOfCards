0b81d452aeadb660431a1ec7b86c9d7ebc1b5d40
sarafiskray <sarafiskray@gmail.com>
2021-07-07 13:58:17 -0400

    first commit (mac)

---
+++ Card.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace DeckOfCards
+{
+    class Card
+    {
+        enum Suit
+        {
+            Diamonds,
+            Clubs,
+            Hearts,
+            Spades
+        }
+
+        enum Rank 
+        {
+            Ace = 1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+    }
+}
