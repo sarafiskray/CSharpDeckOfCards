34d4846c6935a4e2b332d997187b434b41bfd0bd
sarafiskray <sarafiskray@gmail.com>
2021-07-07 19:35:53 -0400

    added shuffle

--- Deck.cs
+++ Deck.cs
@@ -7,10 +7,10 @@ namespace DeckOfCards
     {
 
         
-        public List<Card> cards {get; set;}
-        //I considered using a Stack to represent the cards
-        //would have made it easy to see the top card
-        //but harder to shuffle
+        public List<Card> cards {get;}
+        //List is ordered and resizes dynamically
+        //Stack would be nice for drawing card, but overall List makes more sense
+        
 
         public Deck() {
             //I considered using Enum.GetValues() to loop through the suits and ranks.
@@ -30,10 +30,28 @@ namespace DeckOfCards
         }
 
         public Card drawCard() {
+            //I chose index cards.Count - 1 instead of 0
+            //to represent the top of the deck
             Card topCard = cards[cards.Count - 1];
             cards.Remove(topCard);
             Console.WriteLine(topCard.ToString());
             return topCard;
+        }
+
+        //I decided to use random so we get a different deck each time
+        //additionally, I wanted to do it in one pass for optimal efficiency
+        public void shuffle() {
+            Random rand = new Random();
+            for (int i = 0; i < cards.Count - 1; i++) {
+                int randIndex = rand.Next(i, cards.Count);
+                Card temp = cards[i];
+                cards[i] = cards[randIndex];
+                cards[randIndex] = temp;
+            }
+        }
+
+        public List<Card> showRestOfDeck() {
+            return cards;
         }
 
 
--- Program.cs
+++ Program.cs
@@ -7,15 +7,56 @@ namespace DeckOfCards
     {
         static void Main(string[] args)
         {
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Deck of Cards Simulator in C#\r");
+            Console.WriteLine("-----------------------------\n");
+            string choice = "";
+
+            // Ask the user to choose an option.
+            do {
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\tn - Get a new deck");
+                Console.WriteLine("\ts - Shuffle the current deck");
+                Console.WriteLine("\td - Draw top card from deck");
+                Console.WriteLine("\tr - See rest of cards in deck");
+                Console.WriteLine("\tq - Quit");
+                Console.Write("What would you like to do? ");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "n":
+                        //new deck
+                        Console.WriteLine("you chose a");
+                        break;
+                    case "s":
+                        //shuffle
+                        Console.WriteLine("you chose s");
+                        break;
+                    case "d":
+                        //draw card
+                        Console.WriteLine("you chose m");
+                        break;
+                    case "r":
+                        //see rest of cards
+                        Console.WriteLine("you chose d");
+                        break;
+                }
+            } while (choice != "q");
             //Card testCard = new Card(Rank.Eight, Suit.Hearts);
             //Console.WriteLine(testCard.ToString());
             Deck testDeck = new Deck();
-            //foreach (Card card in testDeck.cards) {
-            //    Console.WriteLine(card.ToString());
-            //}
+        
             testDeck.drawCard();
             testDeck.drawCard();
             testDeck.drawCard();
+            foreach (Card card in testDeck.cards) {
+                Console.WriteLine(card.ToString());
+            }
+            Console.WriteLine("...");
+            testDeck.shuffle();
+            foreach (Card card in testDeck.cards) {
+                Console.WriteLine(card.ToString());
+            }
             
         }
     }
